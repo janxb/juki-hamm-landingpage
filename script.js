07bc63fba8f2cf689c0790e7dc9453cfc3655a99
Jan Brodda <git@janbrodda.de>
2018-08-09 22:47:37 +0200

    add hash route handling and whatsapp content

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
 $("document").ready(function () {
+    var app = new Vue({
+        el: '#app',
+        data: {
+            views: {
+                main: true,
+                whatsapp: false,
+                calendar: false
+            }
+        },
+        methods: {
+            switchView: function (event, view) {
+                if (this.views.hasOwnProperty(view)) {
+                    for (var tmpView in this.views) {
+                        if (this.views.hasOwnProperty(tmpView))
+                            this.views[tmpView] = false;
+                    }
+                    this.views[view] = true;
+                    window.location.hash = view;
+                }
+            },
+            hashChanged: function () {
+                if (window.location.hash) {
+                    this.switchView(
+                        null, window.location.hash.replace('#', '')
+                    );
+                }
+            }
+        },
+        watch: {},
+        mounted() {
+            this.hashChanged();
+        }
+    });
 
-});
+    window.onhashchange = function (hash) {
+        app.hashChanged();
+    };
+})
+;
