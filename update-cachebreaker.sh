7f50a1a7dc67d00fa8cfd8f8b23bf3862ecaa2c2
Jan Brodda <git@janbrodda.de>
2018-08-09 22:48:12 +0200

    fix special characters in project directory path

--- update-cachebreaker.sh
+++ update-cachebreaker.sh
@@ -4,8 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 function cachebreaker {
 	FILE=$DIR/$1
-	hash=`md5sum ${FILE} | awk '{ print $1 }'`
-	sed -i 's/'$1'?v=.*"/'$1'?v='$hash'"/g' $DIR/$2
+	hash=`md5sum "${FILE}" | awk '{ print $1 }'`
+	sed -i 's/'$1'?v=.*"/'$1'?v='$hash'"/g' "$DIR/$2"
 	echo "Updated cachebreaker for file $1 : $hash"
 }
 
