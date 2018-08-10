108723ee183f4677dee571288c1edfe67400d25b
Jan Brodda <git@janbrodda.de>
2018-08-10 09:41:37 +0200

    - added recurring calendar events - landscape logo has sticky position now - views now have unique location hashs for bookmarks

--- script.js
+++ script.js
@@ -6,17 +6,41 @@ $("document").ready(function () {
                 main: true,
                 whatsapp: false,
                 calendar: false
+            },
+            calendar: {
+                recurringEvents: [
+                    {
+                        title: "JuKi-Treff",
+                        description: "Jeden Dienstag von 18-20 Uhr treffen wir uns in der Jugendkirche und planen die nächsten Gottesdienste und Aktionen. Komm vorbei, wir freuen uns immer über neue Gesichter!",
+                        responsible: "Katrin Berger"
+                    },
+                    {
+                        title: "Jugendtreff",
+                        description: "Jeden Mittwoch von 16-18 Uhr sind wir für dich in der Jugendkirche. Hier kannst du chillen, quatschen oder andere coole Dinge machen. Komm gerne vorbei, es gibt keinen Haken!",
+                        responsible: "Katrin Burghardt"
+                    },
+                    {
+                        title: "Nachwuchs Musik und Technik",
+                        description: "Du hast Lust im Jugendgottesdienst Musik zu machen, oder interessierst dich für Ton- und Lichttechnik? Dann bist du bei uns richtig! Wir sind jeden Donnerstag von 16-18 Uhr in der Jugendkirche und geben dir Raum zum Lernen und Ausprobieren.",
+                        responsible: "Tobias Pelmke und Ulrike Egermann"
+                    }
+                ]
             }
         },
         methods: {
             switchView: function (event, view) {
+                var viewHash = view;
+                if (view === '') {
+                    view = 'main';
+                }
+
                 if (this.views.hasOwnProperty(view)) {
                     for (var tmpView in this.views) {
                         if (this.views.hasOwnProperty(tmpView))
                             this.views[tmpView] = false;
                     }
                     this.views[view] = true;
-                    window.location.hash = view;
+                    window.location.hash = viewHash;
                 }
             },
             hashChanged: function () {
@@ -29,6 +53,10 @@ $("document").ready(function () {
         },
         watch: {},
         mounted() {
+            if (!window.location.hash) {
+                window.location.hash = 'dummy';
+                window.location.hash = '';
+            }
             this.hashChanged();
         }
     });
